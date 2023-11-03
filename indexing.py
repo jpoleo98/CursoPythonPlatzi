a787bc97e2c0e6ab83b9b53727b5b8c8cea829bb
José Poleo <jose.poleo.2a@gmail.com>
2023-11-02 20:25:50 -0400

    Ejercicios Adicionales

---
+++ indexing.py
@@ -0,0 +1,3 @@
+text = 'Hola Crack'
+print(text[0])
+print(text[0:5])
---
+++ strings.py
@@ -0,0 +1,16 @@
+text = 'Ella sabe programar'
+
+print('Ella' in text);
+
+if ('Ella' in text):
+    print('si esta')
+else:
+    print('caida')
+
+print(len(text))
+
+print(text.upper())
+print(text.lower())
+print(text.count('a'))
+print(text.startswith('Ella'))
+print(text.replace('sabe','mucho'))
