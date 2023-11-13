31bcf442226f7f359f780926d960ae0ea83ddefa
José Poleo <jose.poleo.2a@gmail.com>
2023-11-12 20:56:34 -0400

    Refactor: Al proyecto se le anexo funciones

--- proyecto.py
+++ proyecto.py
@@ -1,16 +1,7 @@
 import random
 
-opcionesTupla = (1,2,3)
-
-turnos = 10
-i = 1
-pwin=0
-cpuwin=0
-
-turnosWins=3
-
-while i<=turnos:
-    print(f'Ronda {i} de {turnos}')
+def choise():
+    opcionesTupla = (1,2,3)
     while True:
         player = input('1.Piedra - 2.Papel - 3.Tijera:');
         if player.isdigit():
@@ -20,7 +11,10 @@ while i<=turnos:
             print('introduzca una opcion valida')
             
     cpu = random.choice(opcionesTupla);
+    
+    return player, cpu
 
+def checkRules(player,cpu,pwin,cpuwin,continuar):
     if player >= 1 and player<=3:
         if player==cpu:
             print('Empate')
@@ -32,13 +26,41 @@ while i<=turnos:
             cpuwin+=1
     else:
         print('introduzca una opcion valida')
-        continue
+        continuar = 'continue'
+    
+    return pwin,cpuwin,continuar
 
+def checkwinner(pwin,cpuwin,turnosWins):
     if turnosWins == pwin:
         print('Ganador Player')
-        break
-    
+        return 'break'
+        
     if turnosWins == cpuwin:
         print('Gano CPU')
-        break
-    i+=1
+        return 'break'
+
+
+def main():
+    pwin=0
+    cpuwin=0
+    turnos = 10
+    i = 1   
+    turnosWins=3
+    continuar = ''
+
+    while i<=turnos:
+        print(f'Ronda {i} de {turnos}')
+
+        player, cpu = choise()
+        pwin, cpuwin, validadError = checkRules(player,cpu,pwin,cpuwin,continuar)
+        if validadError == 'continue':
+            continue
+        
+        ganador = checkwinner(pwin,cpuwin,turnosWins)
+
+        if ganador == 'break':
+            break
+
+        i+=1
+
+main()
